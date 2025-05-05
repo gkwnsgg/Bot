326d3be8a992cfdfa81107c2e4d088c5586c8c33
gkwnsgg <ryanz7@naver.com>
2025-05-05 15:09:02 +0900

    Update Auto_push.sh

--- Auto_push.sh
+++ Auto_push.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-cd /home/ubuntu/Bot || exit 1
+cd /home/ubuntu/Bot
 
 TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
 echo "[$TIMESTAMP] Git 자동 커밋 실행 중..." >> gitlog.txt
 
 git pull origin main --rebase >> gitlog.txt 2>&1
-if git diff --quit; then
-    echo "[$TIMESTAMP] 변경 사항 없음" >> gitlog.txt
-else
+git rebase origin/main >> gitlog.txt 2>&1
+
+if ! git diff --quit; then
     git add subs.json >> gitlog.txt 2>&1
     git commit -m "자동 커밋: $TIMESTAMP" >> gitlog.txt 2>&1
     git push origin main >> gitlog.txt 2>&1
+    echo "[$TIMESTAMP] 변경 사항 없음" >> gitlog.txt
+else
     echo "[$TIMESTAMP] Git 자동 커밋 완료" >> gitlog.txt
 fi
