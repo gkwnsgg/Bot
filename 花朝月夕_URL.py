d155a2c7d8e74c67b99a014f5c11b8f4dd77c0fb
gkwnsgg <ryanz7@naver.com>
2025-05-10 19:35:16 +0900

    Update 花朝月夕_URL.py

--- 花朝月夕_URL.py
+++ 花朝月夕_URL.py
@@ -1,5 +1,5 @@
 from 花朝月夕_Token import SahYang_User
 
-URL_SahYang = f"https://api.chzzk.naver.com/service/v1/channels/{SahYang_User}"
-URL_SahYang0 = f"https://api.chzzk.naver.com/polling/v3/channels/{SahYang_User}/live-status?includePlayerRecommendContent=true"
-URL_Notion = f"https://www.notion.so/1ec12ee7d80780d2a991e30acb657904?v=1ec12ee7d807802bb555000c57b6ad64&pvs=4"
+URL_SahYang=f"https://api.chzzk.naver.com/service/v1/channels/{SahYang_User}"
+URL_SahYang0=f"https://api.chzzk.naver.com/polling/v3/channels/{SahYang_User}/live-status?includePlayerRecommendContent=true"
+URL_Notion=f"https://www.notion.so/1ec12ee7d80780d2a991e30acb657904?v=1ec12ee7d807802bb555000c57b6ad64&pvs=4"
