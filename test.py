baacad02a10efc6805c7b22070cc6f6d5a45ac71
gkwnsgg <ryanz7@naver.com>
2025-05-14 20:18:15 +0900

    Fix

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 
-font_path = "./GodoM.otf"
+font_path = "./Godo M.otf"
 font_prop = fm.FontProperties(fname=font_path)
 
 print("폰트 이름:", font_prop.get_name())
 
 plt.rcParams['font.family'] = font_prop.get_name()
 plt.rcParams['axes.unicode_minus'] = False
+plt.plot([1, 2, 3], [1, 4, 9])
+plt.title("한글 테스트")
+plt.savefig("test_plot.png")
